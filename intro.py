9b690919734f8e4a6394f34d9e4ed88c57e98fa3
Jacky <jacky.ck@gmail.com>
2023-12-09 14:23:37 +0000

    first commit

---
+++ intro.py
@@ -0,0 +1,15 @@
+print('Hello')
+
+a = 1
+a += 2
+print(a)
+
+
+## AppData\Local\Microsoft\WindowsApps\python3
+
+# C:\Users\jacky\AppData\Local\Programs\Python\Python312\python.exe
+
+# C:\Users\jacky\AppData\Local\Programs\Python\Python312\python.exe -m pip install --upgrade pip
+
+# pip install 
+# C:\Users\jacky\AppData\Local\Programs\Python\Python312\python.exe -m pip install pandas
